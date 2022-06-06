9d75982b59287a1babd4fdcda86543809ff2f0cf
Kristjanmn <kristjanmn@hotmail.com>
2022-06-06 14:59:32 +0300

    Updated app-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {LoadingService} from "./service/loading.service";
+import {environment} from "../environments/environment";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  title = environment.defaultTitle;
+
+  constructor(
+    public loadingService: LoadingService
+  ) {}
+
+  ngOnInit() {
+    // will add some xsrf logic here, when i get to that part
+  }
 }
