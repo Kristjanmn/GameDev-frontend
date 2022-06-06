5ab4ed43b4a7856534702ee5453f5e116824d63d
Kristjanmn <kristjanmn@hotmail.com>
2022-06-06 13:42:59 +0300

    Added Angular Material and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {HttpClientModule, HttpClientXsrfModule} from "@angular/common/http";
+import { AppRoutingModule } from './app-routing.module';
+import {MaterialModule} from "./material/material.module";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    HttpClientXsrfModule,
+    AppRoutingModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
