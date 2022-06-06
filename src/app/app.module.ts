eef96f6d53ea821ad8a95bf5e39c0b3a2cdf7cda
Kristjanmn <kristjanmn@hotmail.com>
2022-06-06 14:33:05 +0300

    Created page-not-found component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,16 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {AppHeaderComponent} from "./component/app-header/app-header.component";
 import {IndexComponent} from "./component/index/index.component";
+import {PageNotFoundComponent} from "./component/page-not-found/page-not-found.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     AppHeaderComponent,
-    IndexComponent
+    IndexComponent,
+
+    // error 404 - page not found
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
