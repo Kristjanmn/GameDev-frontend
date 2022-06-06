b246bbb4b54cc12880b93b6d4045fb29e217350e
Kristjanmn <kristjanmn@hotmail.com>
2022-06-06 15:08:43 +0300

    Updated app-routing.module.ts with PageNotFoundComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {Routes} from "@angular/router";
+import {RouterModule, Routes} from "@angular/router";
 import {IndexComponent} from "./component/index/index.component";
+import {PageNotFoundComponent} from "./component/page-not-found/page-not-found.component";
 
 const routes: Routes = [
   // Index
-  {path: '', component: IndexComponent}
+  {path: '', component: IndexComponent},
+
+  // page not found - error 404
+  {path: '**', pathMatch: 'full', component: PageNotFoundComponent}
 ]
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
