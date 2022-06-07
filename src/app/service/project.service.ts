1a3e3b2f5471ae0f238b900d09dcc3abdde9510e
Kristjanmn <kristjanmn@hotmail.com>
2022-06-07 17:24:19 +0300

    Created project.service.ts

---
+++ src/app/service/project.service.ts
@@ -0,0 +1,32 @@
+import {Injectable} from "@angular/core";
+import {environment} from "../../environments/environment";
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
+import {CustomResponse} from "../model/customResponse";
+import {Project} from "../model/project";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectService {
+  private readonly baseUrl = environment.backendUrl + "/api/project"
+
+  constructor(private http: HttpClient,
+              private router: Router) {
+  }
+
+  getByProjectId(projectId: string): Project {
+    let project;
+    this.http.get<CustomResponse>(`${this.baseUrl}/getByProjectId/${projectId}`)
+      .subscribe(response => {
+        if (!response.success || response.object == null)
+          this.router.navigate(['']);
+        project = <Project>response.object;
+      });
+    return <Project><unknown>project;
+  }
+
+  getProjectDialogs(projectDatabaseId: string): void {
+    //
+  }
+}
