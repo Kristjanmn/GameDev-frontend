1515d682adf654fc1993f8c81e86e5e17827de92
Kristjanmn <kristjanmn@hotmail.com>
2022-06-15 02:01:44 +0300

    Added redirect to PageNotFound component

--- src/app/component/page-not-found/page-not-found.component.ts
+++ src/app/component/page-not-found/page-not-found.component.ts
@@ -1,7 +1,22 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {Router} from "@angular/router";
+import {Title} from "@angular/platform-browser";
 
 @Component({
   selector: 'page-not-found',
   templateUrl: './page-not-found.component.html'
 })
-export class PageNotFoundComponent {}
+export class PageNotFoundComponent implements OnInit {
+  redirectTime = 5;
+
+  constructor(private router: Router,
+              private titleService: Title) {
+  }
+
+  ngOnInit(): void {
+    this.titleService.setTitle("Page not found")
+    setTimeout(() => {
+      this.router.navigate(['']);
+    }, this.redirectTime * 1000);
+  }
+}
