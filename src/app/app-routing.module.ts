f6720679fb670c038243bfff336e93b868c8ea96
Kristjanmn <kristjanmn@hotmail.com>
2022-06-15 02:04:41 +0300

    Added project paths to app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,17 @@ import { CommonModule } from '@angular/common';
 import {RouterModule, Routes} from "@angular/router";
 import {IndexComponent} from "./component/index/index.component";
 import {PageNotFoundComponent} from "./component/page-not-found/page-not-found.component";
+import {NewProjectComponent} from "./component/new-project/new-project.component";
+import {ProjectComponent} from "./component/project/project.component";
 
 const routes: Routes = [
   // Index
   {path: '', component: IndexComponent},
+  {path: 'new-project', component: NewProjectComponent},
+  {path: ':id', component: ProjectComponent},
 
   // page not found - error 404
+  //{path: 'error/404', component: PageNotFoundComponent},
   {path: '**', pathMatch: 'full', component: PageNotFoundComponent}
 ]
 
