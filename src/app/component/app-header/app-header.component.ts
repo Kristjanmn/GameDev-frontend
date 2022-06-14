ede2bd0d5ccef9aa2b1bcb9d3ced9e801bc6a393
Kristjanmn <kristjanmn@hotmail.com>
2022-06-15 02:02:15 +0300

    Created app-header component

--- src/app/component/app-header/app-header.component.ts
+++ src/app/component/app-header/app-header.component.ts
@@ -1,7 +1,20 @@
 import {Component} from "@angular/core";
+import {ActivatedRoute, Router} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
+import {LoadingService} from "../../service/loading.service";
 
 @Component({
   selector: 'app-header',
   templateUrl: './app-header.component.html'
 })
-export class AppHeaderComponent {}
+export class AppHeaderComponent {
+
+  constructor(private route: ActivatedRoute,
+              private router: Router,
+              private http: HttpClient,
+              private loadingService: LoadingService) {}
+
+  toIndex(): void {
+    this.router.navigate(['']);
+  }
+}
