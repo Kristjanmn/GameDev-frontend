5bb9f0fcfe8bba516203568ad47463364483b5c7
Kristjanmn <kristjanmn@hotmail.com>
2022-06-17 02:52:25 +0300

    Updated project.component.ts with improved subscription in 'getProject' and 'getDialogs' functions

--- src/app/component/project/project.component.ts
+++ src/app/component/project/project.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from "@angular/core";
 import {Project} from "../../model/project";
 import {ActivatedRoute, Router} from "@angular/router";
 import {ProjectService} from "../../service/project.service";
+import {Dialog} from "../../model/dialog";
 
 @Component({
   selector: 'project',
@@ -9,6 +10,7 @@ import {ProjectService} from "../../service/project.service";
 })
 export class ProjectComponent implements OnInit{
   project?: Project;
+  dialogs?: Dialog[];
 
   constructor(private router: Router,
               private route: ActivatedRoute,
@@ -16,15 +18,37 @@ export class ProjectComponent implements OnInit{
   }
 
   ngOnInit(): void {
-    this.route.queryParams
+    this.route.params
       .subscribe(params => {
-        if (params["projectId"]) {
-          this.getProject(params["projectId"]);
+        if (params["id"]) {
+          this.getProject(params["id"]);
         } //else this.router.navigate(['error/404']);
       });
   }
 
   getProject(projectId: string): void {
-    this.project = this.projectService.getByProjectId(projectId);
+    this.projectService.getByProjectId(projectId)
+      .subscribe(response => {
+        if (!response.success) {
+          // display error
+          // redirect
+        } else {
+          this.project = <Project>response.object;
+          this.getDialogs(this.project.id);
+        }
+      });
+  }
+
+  /**
+   * Project's database ID, NOT projectId
+   *
+   * @param projectId project's database ID
+   */
+  getDialogs(projectId: string): void {
+    this.projectService.getProjectDialogs(projectId)
+      .subscribe(response => {
+        if (response.success) this.dialogs = <Dialog[]>response.object;
+        else this.dialogs = [];
+      });
   }
 }
