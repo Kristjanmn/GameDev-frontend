5b57e1e7d4eb4f421b8e353412cb4c7f1a05c788
Kristjanmn <kristjanmn@hotmail.com>
2022-06-17 13:45:15 +0300

    project.component.ts Added 'quests', 'scripts' and 'cues'. Added constructors for dialog windows for creating 'dialog', 'quest', 'script' and 'cue' objects.

--- src/app/component/project/project.component.ts
+++ src/app/component/project/project.component.ts
@@ -1,8 +1,12 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, Inject, OnInit} from "@angular/core";
 import {Project} from "../../model/project";
 import {ActivatedRoute, Router} from "@angular/router";
 import {ProjectService} from "../../service/project.service";
 import {Dialog} from "../../model/dialog";
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import {Quest} from "../../model/quest";
+import {Script} from "../../model/script";
+import {Cue} from "../../model/cue";
 
 @Component({
   selector: 'project',
@@ -11,6 +15,9 @@ import {Dialog} from "../../model/dialog";
 export class ProjectComponent implements OnInit{
   project?: Project;
   dialogs?: Dialog[];
+  quests?: Quest[];
+  scripts?: Script[];
+  cues?: Cue[];
 
   constructor(private router: Router,
               private route: ActivatedRoute,
@@ -50,5 +57,93 @@ export class ProjectComponent implements OnInit{
         if (response.success) this.dialogs = <Dialog[]>response.object;
         else this.dialogs = [];
       });
+  }
+}
+
+// Dialog
+
+export interface NewDialogData {
+  dialogId: string;
+  comment: string;
+}
+
+@Component({
+  selector: 'new-dialog-dialog',
+  templateUrl: './new-dialog-dialog.html'
+})
+export class NewDialogDialog {
+  constructor(
+    public dialogRef: MatDialogRef<NewDialogDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: NewDialogData
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
+
+// Quest
+
+export interface NewQuestData {
+  dialogId: string;
+  comment: string;
+}
+
+@Component({
+  selector: 'new-quest-dialog',
+  templateUrl: './new-quest-dialog.html'
+})
+export class NewQuestDialog {
+  constructor(
+    public dialogRef: MatDialogRef<NewQuestDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: NewQuestData
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
+
+// Script
+
+export interface NewScriptData {
+  name: string;
+  comment: string;
+}
+
+@Component({
+  selector: 'new-script-dialog',
+  templateUrl: './new-script-dialog.html'
+})
+export class NewScriptDialog {
+  constructor(
+    public dialogRef: MatDialogRef<NewScriptDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: NewScriptData
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
+
+// Cue
+
+export interface NewCueData {
+  dialogId: string;
+  comment: string;
+}
+
+@Component({
+  selector: 'new-cue-dialog',
+  templateUrl: './new-cue-dialog.html'
+})
+export class NewCueDialog {
+  constructor(
+    public dialogRef: MatDialogRef<NewCueDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: NewCueData
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 }
