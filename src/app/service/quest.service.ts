96e6da4b39345988a9fe87c7ba43e4f33abef66c
Kristjanmn <kristjanmn@hotmail.com>
2022-08-04 10:04:03 +0300

    Added functions QuestService.checkQuestIdAvailable_questDatabaseId, -.getQuestById and -.testSave

--- src/app/service/quest.service.ts
+++ src/app/service/quest.service.ts
@@ -5,6 +5,7 @@ import {Observable} from "rxjs";
 import {CustomResponse} from "../model/customResponse";
 import {Quest} from "../model/quest";
 import {QuestPhase} from "../model/questPhase";
+import {Script} from "../model/script";
 
 @Injectable({
   providedIn: 'root'
@@ -27,7 +28,33 @@ export class QuestService {
     return this.http.get<CustomResponse>(`${this.baseUrl}/checkIdAvailable/${questId}`);
   }
 
+  checkQuestIdAvailable_questDatabaseId(questDatabaseId: string, questId: string) {
+    return this.http.get<CustomResponse>(`${this.baseUrl}/checkIdAvailable/${questDatabaseId}/${questId}`);
+  }
+
   saveQuest(quest: Quest): Observable<CustomResponse> {
+    return this.http.post<CustomResponse>(`${this.baseUrl}/saveQuest`, quest);
+  }
+
+  getQuestById(questDatabaseId: string): Observable<CustomResponse> {
+    return this.http.get<CustomResponse>(`${this.baseUrl}/getById/${questDatabaseId}`);
+  }
+
+  testSave(): Observable<CustomResponse> {
+    let quest = new class implements Quest {
+      id: string = "";
+      questId: string = "test_quest";
+      title: string = "test quest title";
+      phases: QuestPhase[] = [new class implements QuestPhase {
+        comment: string = "test phase comment";
+        description: string = "test phase description";
+        id: string = "";
+        phaseId: string = "test_phase_1";
+        script: Script[] = [];
+        zOrder: number= 0;
+      }];
+      comment: string = "test quest comment";
+    }
     return this.http.post<CustomResponse>(`${this.baseUrl}/saveQuest`, quest);
   }
 }
