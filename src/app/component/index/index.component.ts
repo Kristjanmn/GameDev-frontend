b1b2f851b2bed456ab782774f066ccfea11657fd
Kristjanmn <kristjanmn@hotmail.com>
2022-08-04 10:08:10 +0300

    Added function and button to go to new Project form

--- src/app/component/index/index.component.ts
+++ src/app/component/index/index.component.ts
@@ -1,7 +1,17 @@
 import {Component} from "@angular/core";
+import {Router} from "@angular/router";
+import {environment} from "../../../environments/environment";
 
 @Component({
   selector: 'index',
   templateUrl: './index.component.html'
 })
-export class IndexComponent {}
+export class IndexComponent {
+
+  constructor(private router: Router) {
+  }
+
+  toNewProject(): void {
+    this.router.navigate([environment.newProjectUrl]);
+  }
+}
