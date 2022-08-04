b93a6836e979dcde94d163795c370705b3a12b35
Kristjanmn <kristjanmn@hotmail.com>
2022-08-04 10:41:43 +0300

    Updated app.module.ts with ProjectFormComponent, Dialog-, Quest-, Script-, CueFormComponent, TabsComponent and TabComponent declarations and DragDropModule import.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,29 +5,41 @@ import { AppComponent } from './app.component';
 import {HTTP_INTERCEPTORS, HttpClientModule, HttpClientXsrfModule} from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import {MaterialModule} from "./material/material.module";
+import {DragDropModule} from "@angular/cdk/drag-drop";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {AppHeaderComponent} from "./component/app-header/app-header.component";
 import {IndexComponent} from "./component/index/index.component";
 import {PageNotFoundComponent} from "./component/page-not-found/page-not-found.component";
 import {
+  CueFormComponent,
+  DialogFormComponent,
   NewCueDialog,
   NewDialogDialog,
   NewQuestDialog,
   NewScriptDialog,
-  ProjectComponent
+  ProjectComponent, ProjectFormComponent, QuestFormComponent, ScriptFormComponent
 } from "./component/project/project.component";
 import {CookieService} from "ngx-cookie-service";
 import {NewProjectComponent} from "./component/new-project/new-project.component";
 import {AuthInterceptor} from "./service/auth-interceptor";
+import {TabComponent} from "./component/tabs/tab.component";
+import {TabsComponent} from "./component/tabs/tabs.component";
 
 @NgModule({
   declarations: [
     AppComponent,
+    TabComponent,
+    TabsComponent,
     AppHeaderComponent,
     IndexComponent,
     NewProjectComponent,
     ProjectComponent,
+    ProjectFormComponent,
+    DialogFormComponent,
+    QuestFormComponent,
+    ScriptFormComponent,
+    CueFormComponent,
     NewDialogDialog,
     NewQuestDialog,
     NewScriptDialog,
@@ -44,7 +56,8 @@ import {AuthInterceptor} from "./service/auth-interceptor";
     MaterialModule,
     BrowserAnimationsModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    DragDropModule
   ],
   providers: [
     CookieService,
